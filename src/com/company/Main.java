672949edfb8044d1493401b3527f90d9743d23d5
John Gomez <jgomez818@gmail.com>
2015-11-18 05:10:32 -0500

    fixed some bugs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         //Iterative because you gotta always keep your roots!
-        System.out.println(isEvenWordIter("appas"));
-        System.out.println(isEvenWordIter("potato"));
-        System.out.println(isEvenWordIter("appeases"));
-        System.out.println(isEvenWord("appeasesed"));
-        System.out.println(isEvenWord("appleale"));
-        System.out.println(isEvenWord("firetrucks!"));
-        System.out.println(isEvenWord("momo"));
-        System.out.println(isEvenWord("newton"));
+//        System.out.println(isEvenWordIter("appas"));
+//        System.out.println(isEvenWordIter("potato"));
+//        System.out.println(isEvenWordIter("appeases"));
+//        System.out.println(isEvenWord("appeasesed"));
+//        System.out.println(isEvenWord("appleale"));
+//        System.out.println(isEvenWord("firetrucks!"));
+        System.out.println("MOMOO: "+isEvenWord("momoooo"));
+     //   System.out.println(isEvenWord("newton"));
         // more test cases would be good
     }
 
@@ -33,18 +33,21 @@ public class Main {
         return false;
     }
 
-    public static boolean isEvenWord(String word){
+    public static boolean isEvenWord(String word) {
         //base case
-        if(word.length() == 0 ){
+        if (word.length() == 0 || word == null) {
             return true;
         } else {
-            String remaining = word.substring(2);
-            String firstLetter = String.valueOf(word.charAt(0));
-            if(remaining.contains(firstLetter)){
-                word.replace(firstLetter,"");
-                return isEvenWordIter(word);
+            char firstletter = word.charAt(0);
+            String rest = word.substring(1);
+
+            if (rest.contains("" + firstletter)) {
+                word =  rest.replaceFirst("" + firstletter, "");
+                System.out.println(word);
+                return isEvenWord(word);
             }
-            return false;
+
         }
+            return false;
     }
 }
